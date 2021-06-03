04669ac2413cc0013d94cf7164c082490f43ec3c
Dean Agan <agandfr@gmail.com>
2021-06-03 20:08:28 +1000

    Adding demo for using openpyxl

---
+++ excel_reading.py
@@ -0,0 +1,14 @@
+import openpyxl
+
+wb = openpyxl.load_workbook('Book.xlsx')
+
+print(wb.sheetnames)
+
+sheet = wb['Sheet1']
+print(sheet['A1'])
+print(sheet['A1'].value)
+
+
+# Printing column A's first 3 rows
+for i in range(1, 4):
+    print(i, sheet.cell(row=i, column=1).value)
